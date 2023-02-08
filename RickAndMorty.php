ae1cfb25833e54b17a80ad4807a9d3e58864d2dd
glip <nork1337@gmail.com>
2023-02-08 02:58:42 -0300

    Add docblock and comments

--- RickAndMorty.php
+++ RickAndMorty.php
@@ -16,6 +16,14 @@ class RickAndMorty
         ]);
     }
 
+    /**
+     * Get data based on the input type from the API, with optional parameters for pagination.
+     *
+     * @param string $type The main endpoint call
+     * @param int $page The page number to retrieve
+     * @param int $pageSize The number of items per page
+     * @return array An array of data
+     */
     public function fetchData(string $type = 'character', int $page = 1, int $pageSize = 20): array 
     {
         $query = [
@@ -30,6 +38,13 @@ class RickAndMorty
         return $data['results'];
     }
 
+    /**
+     * Get characters from the API, with optional parameters for pagination.
+     *
+     * @param int $page The page number to retrieve
+     * @param int $pageSize The number of items per page
+     * @return array An array of characters
+     */
     public function getCharacters (int $page = 1, int $pageSize = 20): array
     {
         return $this->fetchData('character', $page, $pageSize);
