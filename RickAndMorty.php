02f0476573d1ff0ec34e06f310ce6fb1d5d0a086
glip <nork1337@gmail.com>
2023-02-08 02:50:44 -0300

    Add getCharacters function on RickAndMorty class

--- RickAndMorty.php
+++ RickAndMorty.php
@@ -15,4 +15,24 @@ class RickAndMorty
             'timeout' => 2.0,
         ]);
     }
+
+    /**
+     * Get a list of characters from the API, with optional parameters for pagination.
+     *
+     * @param int $page The page number to retrieve
+     * @param int $pageSize The number of items per page
+     * @return array An array of characters
+     */
+    public function getCharacters(int $page = 1, int $pageSize = 20): array
+    {
+        $query = [
+            'page' => $page,
+            'pageSize' => $pageSize,
+        ];
+        $response = $this->client->get('character', [
+            'query' => $query,
+        ]);
+        $characters = json_decode($response->getBody(), true);
+        return $characters['results'];
+    }
 }
